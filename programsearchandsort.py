605766ce1e91477e0ec711e233f639e328195622
Rigo Alcover <rodigomartinalcover@gmail.com>
2022-01-25 12:08:09 -0500

    Update programsearchandsort.py

--- programsearchandsort.py
+++ programsearchandsort.py
@@ -3,3 +3,19 @@
 # Advanced Python Programming
 # 01/25/2022
 
+class customlinkedlist:
+    def __init__(self, value, nextnode=None):
+        self.value = value
+        self.nextnode = nextnode
+        
+node1 = customlinkedlist("1")
+node2 = customlinkedlist("7")
+node3 = customlinkedlist("4")
+node4 = customlinkedlist("2")
+node5 = customlinkedlist("9")
+node6 = customlinkedlist("8")
+node7 = customlinkedlist("10")
+node8 = customlinkedlist("-4")
+node9 = customlinkedlist("0")
+node10 = customlinkedlist("3")
+
