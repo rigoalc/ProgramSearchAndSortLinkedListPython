e99f5fb577742056fcf1eec159ecbbc0116dde67
Rigo Alcover <rodigomartinalcover@gmail.com>
2022-01-28 09:37:22 -0500

    One file

--- searchandsortlinked.py
+++ searchandsortlinked.py
@@ -1,22 +1,41 @@
-
-class Nodes:
-    def __init__(self, value):
-        self.value = value
-        self.nextNode = None
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
         
-class linkedList:
-    def __init__(self):
-        self.head = None
-      
+    def traverse(self):
+        print("{} -> ".format(self.data),end='')
+        if self.next:
+            self.next.traverse()
+        else:
+            print('NULL')
+            
+            
+node1 = Node("1")
+node2 = Node("7")
+node3 = Node("4")
+node4 = Node("2")
+node5 = Node("9")
+node6 = Node("8")
+node7 = Node("10")
+node8 = Node("-4")
+node9 = Node("0")
+node10 = Node("3")
+
+node1.nextnode = node2
+node2.nextnode = node3
+node3.nextnode = node4
+node4.nextnode = node5
+node5.nextnode = node6
+node6.nextnode = node7
+node7.nextnode = node8
+node9.nextnode = node10
+
+
+traverse(node1) 
+node1.traverse()           
+            
+            
+        
+
     
-    def insert(self, value):
-        node = customLikedList(value)
-        if self.head is not None:
-            self.head = node
-            return  
-        currentNode = self.head
-        while True:
-            if currentNode.nextNode is None:
-                currentNode.nextNode = node
-                break
-            currentNode = currentNode.nextNode
